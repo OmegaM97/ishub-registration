0e102f543501d5a7af5778389ed0c3c1863506fe
Omega Melese <omegamelese68@gmail.com>
2026-07-03 00:16:15 +0300

    add requirements.txt and cors allow all

--- back-end/main.py
+++ back-end/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from applications import router as applications_router
 from auth import router as auth_router
 from db import init_db
 
 app = FastAPI(title="ishub-registration API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(applications_router)
